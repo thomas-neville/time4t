e76842f1ac5e240bb31fd2620d119403511ccfd9
Thomas Neville <tnev@uw.edu>
2018-06-26 21:58:15 -0700

    add js files for other pages, styles for button, email link button, look at sticky nav

--- scripts/contact.js
+++ scripts/contact.js
@@ -18,17 +18,3 @@ var app = new Vue({
 		// data for blog
 	}
 })
-
-window.onscroll = function() {scrollFunction()};
-
-var navbar = document.getElementById("navbar");
-
-var sticky = navbar.offsetTop;
-
-function scrollFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky")
-  };
-}
