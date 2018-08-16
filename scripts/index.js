64507e65cfdc6423b8330a543637eb18b512027d
Thomas Neville <tnev@uw.edu>
2018-08-15 18:12:14 -0700

    started canvas game with canvas tag

--- scripts/index.js
+++ scripts/index.js
@@ -16,3 +16,32 @@ var app = new Vue({
 		// data for blog
 	}
 })
+
+// Game development
+var teaBag;
+
+function startGame() {
+    myGameArea.start();
+    teaBag = new component(30, 30, "brown", 10, 120);
+}
+
+var myGameArea = {
+		// TODO query selector for class name of canvas? getElementById does not work
+    canvas : document.getElementById("tea-game"),
+    start : function() {
+        this.canvas.width = 500;
+        this.canvas.height = 500;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    }
+}
+
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    ctx = myGameArea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+}
